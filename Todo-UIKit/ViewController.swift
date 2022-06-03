4d3a7a5d74e6f8742dd485928e5882026b1baf8f
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-06-04 02:58:37 +0900

    use realm

--- Todo-UIKit/ViewController.swift
+++ Todo-UIKit/ViewController.swift
@@ -6,17 +6,31 @@
 //
 
 import UIKit
+import RealmSwift
+
+class Task: Object {
+    @objc dynamic var id: String = UUID().uuidString
+    @objc dynamic var title: String = ""
+    @objc dynamic var orderOfItem: Int = 0
+
+    override class func primaryKey() -> String? {
+        return "id"
+    }
+}
 
 class ViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var editButton: UIBarButtonItem!
 
-    var tasks = ["Shopping", "Homework", "Swimming"]
+    private var tasks: Results<Task>?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.register(TaskCell.nib(), forCellReuseIdentifier: TaskCell.identifier)
+
+        loadTasks()
+        tableView.reloadData()
     }
 
     // MARK: - Action
@@ -24,8 +38,10 @@ class ViewController: UIViewController {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add a new task", message: "", preferredStyle: .alert)
         let addAction = UIAlertAction(title: "Add", style: .default) { _ in
-            // TODO: add task
-
+            // add new task
+            let newTask = Task()
+            newTask.title = textField.text!
+            self.add(task: newTask)
             self.tableView.reloadData()
         }
         let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
@@ -50,19 +66,48 @@ class ViewController: UIViewController {
             editButton.title = "Done"
         }
     }
+
+    private func loadTasks() {
+        guard let realm = try? Realm() else { return }
+        tasks = realm.objects(Task.self).sorted(byKeyPath: "orderOfItem")
+    }
+
+    private func add(task: Task) {
+        guard let realm = try? Realm() else { return }
+        do {
+            try realm.write {
+                realm.add(task)
+            }
+        } catch {
+            print("Error adding the task. \(error)")
+        }
+    }
+
+    private func delete(task: Task) {
+        guard let realm = try? Realm() else { return }
+        do {
+            try realm.write {
+                realm.delete(task)
+            }
+        } catch {
+            print("Error deleting the task. \(error)")
+        }
+    }
 }
 
 // MARK: - UITableViewDataSource
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        tasks.count
+        tasks?.count ?? 1
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TaskCell.identifier, for: indexPath) as? TaskCell else {
             return UITableViewCell()
         }
-        cell.configure(titleText: tasks[indexPath.row])
+        if let task = tasks?[indexPath.row] {
+            cell.configure(titleText: task.title)
+        }
         return cell
     }
 }
@@ -75,12 +120,41 @@ extension ViewController: UITableViewDelegate {
 
     // MARK: - Edit Cell
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        // TODO: move cell in Editing mode
+        // move cell in Editing mode
+        guard let realm = try? Realm() else { return }
+        do {
+            try realm.write {
+                let sourceItem = tasks?[sourceIndexPath.row]
+                let destinationItem = tasks?[destinationIndexPath.row]
+
+                let destinationItemOrder = destinationItem?.orderOfItem
+
+                if sourceIndexPath.row < destinationIndexPath.row {
+                    for index in sourceIndexPath.row ... destinationIndexPath.row {
+                        tasks?[index].orderOfItem -= 1
+                    }
+                } else {
+                    for index in (destinationIndexPath.row ..< sourceIndexPath.row).reversed() {
+                        tasks?[index].orderOfItem += 1
+                    }
+                }
+
+                guard let destOrder = destinationItemOrder else {
+                    fatalError("destinationItemOrder does not exist.")
+                }
+                sourceItem?.orderOfItem = destOrder
+            }
+        } catch {
+            print("Error moving the cell. \(error)")
+        }
     }
 
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // TODO: delete data
+            // delete a task
+            guard let task = tasks?[indexPath.row] else { return }
+            delete(task: task)
+            tableView.reloadData()
         }
     }
 
